ae98fcf5654dfdfc53ef69d1535f585cbb63978b
ihyewon <loobis200172@gmail.com>
2023-04-06 18:50:35 +0900

    수정본

---
+++ app/src/main/java/com/example/bigmap/NaviSDK.java
@@ -0,0 +1,14 @@
+package com.example.bigmap;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NaviSDK extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_navi_sdk);
+    }
+}
