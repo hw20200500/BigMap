2768cc28661aea07960172b26109216ee11be294
jaeyoung <sys4217@naver.com>
2023-04-10 18:13:28 +0900

    api-key : in build.gradle ivitnav_var : 아래 네비게이션 코드 따로 분리해냄

--- app/src/main/java/com/example/bigmap/MainActivity.java
+++ app/src/main/java/com/example/bigmap/MainActivity.java
@@ -1,16 +1,20 @@
 package com.example.bigmap;
 
+import com.example.bigmap.BuildConfig;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.tmapmobility.tmap.tmapsdk.ui.fragment.NavigationFragment;
@@ -23,60 +27,62 @@ public class MainActivity extends AppCompatActivity {
     private NavigationFragment navigationFragment;
     private FragmentManager fragmentManager;
     private FragmentTransaction transaction;
-    private static final String TAG = "TMapUISDKSample";
+
+    private final static String apikey = BuildConfig.Api_key;
+
+    private static final String TAG = "Big_Map";
     private final static String CLIENT_ID = "";
-    private final static String API_KEY = "aN30ZSiyEh53NXC2wXb4d2gP0cAIuP2S1RTH6X10";
+    private final static String API_KEY = apikey;
     private final static String USER_KEY = "";
     boolean isEDC;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE); //타이틀 바 제거 코드 입니다.
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //checkPermission();
+        initnav_var();
 
-        //tmap navi sdk 화면으로 불러오기
-//        initUI();
-//        initUISDK();
-
-//        checkPermission();
-
-        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavi);
-        getSupportFragmentManager().beginTransaction().add(R.id.main_content,new Bottom_Home()).commit();
-        //바텀 네비게이션뷰 안의 아이템 설정
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                switch (menuItem.getItemId()) {
-                    //item을 클릭시 id값을 가져와 FrameLayout에 fragment.xml띄우기
-                    case R.id.item_home:
-                        findViewById(R.id.main_frame).setVisibility(View.INVISIBLE);
-                        findViewById(R.id.main_content).setVisibility(View.VISIBLE);
-                        getSupportFragmentManager().beginTransaction().replace(R.id.main_content, new Bottom_Home()).commit();
-
-                        break;
-                    case R.id.item_star:
-                        findViewById(R.id.main_frame).setVisibility(View.INVISIBLE);
-                        findViewById(R.id.main_content).setVisibility(View.VISIBLE);
-                        getSupportFragmentManager().beginTransaction().replace(R.id.main_content, new Bottom_Favorite()).commit();
-
-                        break;
-                    case R.id.item_viewList:
-                        findViewById(R.id.main_frame).setVisibility(View.VISIBLE);
-                        findViewById(R.id.main_content).setVisibility(View.INVISIBLE);
-                        getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, new BoardFragment()).commit();
-                        break;
-                    case R.id.item_mine:
-                        findViewById(R.id.main_frame).setVisibility(View.VISIBLE);
-                        findViewById(R.id.main_content).setVisibility(View.INVISIBLE);
-                        getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, new MypageFragment()).commit();
-                        break;
-                }
-                return true;
-            }
-
-        });
     }
+        private void initnav_var(){
+            BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavi);
+            getSupportFragmentManager().beginTransaction().add(R.id.main_content,new Bottom_Home()).commit();
+            //바텀 네비게이션뷰 안의 아이템 설정
+            bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                    switch (menuItem.getItemId()) {
+                        //item을 클릭시 id값을 가져와 FrameLayout에 fragment.xml띄우기
+                        case R.id.item_home:
+                            findViewById(R.id.main_frame).setVisibility(View.INVISIBLE);
+                            findViewById(R.id.main_content).setVisibility(View.VISIBLE);
+                            getSupportFragmentManager().beginTransaction().replace(R.id.main_content, new Bottom_Home()).commit();
 
+                            break;
+                        case R.id.item_star:
+                            findViewById(R.id.main_frame).setVisibility(View.INVISIBLE);
+                            findViewById(R.id.main_content).setVisibility(View.VISIBLE);
+                            getSupportFragmentManager().beginTransaction().replace(R.id.main_content, new Bottom_Favorite()).commit();
+
+                            break;
+                        case R.id.item_viewList:
+                            findViewById(R.id.main_frame).setVisibility(View.VISIBLE);
+                            findViewById(R.id.main_content).setVisibility(View.INVISIBLE);
+                            getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, new BoardFragment()).commit();
+                            break;
+                        case R.id.item_mine:
+                            findViewById(R.id.main_frame).setVisibility(View.VISIBLE);
+                            findViewById(R.id.main_content).setVisibility(View.INVISIBLE);
+                            getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, new MypageFragment()).commit();
+                            break;
+                    }
+                    return true;
+                }
+
+            });
+        }
         private void checkPermission() {
 
         if (checkSelfPermission(android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
@@ -107,20 +113,20 @@ public class MainActivity extends AppCompatActivity {
 
 //    tmap Navi SDK 함수 모음 (이전에는 가상 디바이스에서 구현이 되다가 갑자기 다운되서 안되네요.. 그래서 일단 비활성화 해놨습니다.)
 
-//    @Override
-//    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-//        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-//
-//        if (requestCode == 100
-//                && grantResults[0] == PackageManager.PERMISSION_GRANTED
-//                && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
-//            initUI();
-//            initUISDK();
-//        } else {
-//            Toast.makeText(this, "위치 권한이 없습니다.", Toast.LENGTH_SHORT).show();
-//            finish();
-//        }
-//    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == 100
+                && grantResults[0] == PackageManager.PERMISSION_GRANTED
+                && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+            initUI();
+            initUISDK();
+        } else {
+            Toast.makeText(this, "위치 권한이 없습니다.", Toast.LENGTH_SHORT).show();
+            finish();
+        }
+    }
 
     private void initUI() {
         fragmentManager = getSupportFragmentManager();
@@ -131,7 +137,7 @@ public class MainActivity extends AppCompatActivity {
         transaction.add(R.id.tmapUILayout, navigationFragment);
         transaction.commitAllowingStateLoss();
 
-//        isEDC = false;
+        //isEDC = false;
 
 
         //네비게이션 상태 변경 시 callback
