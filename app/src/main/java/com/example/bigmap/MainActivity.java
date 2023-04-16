d4ff3d3d9a17b3c47004c7ebd4d06c8e93181dfe
jaeyoung <sys4217@naver.com>
2023-04-16 19:00:14 +0900

    작업테스트 Dev - 안심주행 버튼 추가(테스트용) 홈,즐겨찾기 터치 인식안함 이유찾아냄(tmap main 화면 사이즈 조정으로 설정가능할듯)
    필요작업 - 위치검색 검색바 추가 > 검색바 위치 추정 + 네비안내 시작버튼추가

--- app/src/main/java/com/example/bigmap/MainActivity.java
+++ app/src/main/java/com/example/bigmap/MainActivity.java
@@ -17,6 +17,7 @@ import androidx.fragment.app.FragmentTransaction;
 
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.skt.tmap.engine.navigation.route.RoutePlanType;
 import com.tmapmobility.tmap.tmapsdk.ui.fragment.NavigationFragment;
 import com.tmapmobility.tmap.tmapsdk.ui.util.TmapUISDK;
 
@@ -143,131 +144,7 @@ public class MainActivity extends AppCompatActivity {
         transaction.add(R.id.tmapUILayout, navigationFragment);
         transaction.commitAllowingStateLoss();
 
-        //isEDC = false;
-
-
-        //네비게이션 상태 변경 시 callback
-//        navigationFragment.setDrivingStatusCallback(new TmapUISDK.DrivingStatusCallback() {
-//
-//            @Override
-//            public void onUserRerouteComplete() {
-//                // 사용자 재탐색 동작 완료 시 호출
-//            }
-//
-//            @Override
-//            public void onStopNavigation() {
-//                // 네비게이션 종료 시 호출
-////                buttonLayout.setVisibility(View.VISIBLE);
-//            }
-//
-//            @Override
-//            public void onStartNavigation() {
-//                Log.e(TAG, "onStartNavigation");
-//                // 네비게이션 시작 시 호출
-//            }
-//
-//            @Override
-//            public void onRouteChanged(int i) {
-//                // 경로 변경 완료 시 호출
-//            }
-//
-//            @Override
-//            public void onPermissionDenied(int i, @Nullable String s) {
-//                // 권한 에러 발생 시 호출
-//            }
-//
-//            @Override
-//            public void onPeriodicRerouteComplete() {
-//                // 정주기 재탐색 동작 완료 시 호출
-//            }
-//
-//            @Override
-//            public void onPeriodicReroute() {
-//                // 정주기 재탐색 발생 시점에 호출
-//            }
-//
-//            @Override
-//            public void onPassedViaPoint() {
-//                // 경유지 통과 시 호출
-//            }
-//
-//            @Override
-//            public void onPassedTollgate(int i) {
-//                // 톨게이트 통과 시 호출
-//                // i 요금
-//            }
-//
-//            @Override
-//            public void onPassedAlternativeRouteJunction() {
-//                // 대안 경로 통과 시 호출
-//            }
-//
-//            @Override
-//            public void onNoLocationSignal(boolean b) {
-//                // GPS 상태 변화 시점에 호출
-//
-//            }
-//
-//            @Override
-//            public void onLocationChanged() {
-//                //위치 갱신 때 마다 호출
-//            }
-//
-//            @Override
-//            public void onFailRouteRequest(@NonNull String errorCode, @NonNull String errorMsg) {
-//                //경로 탐색 실패 시 호출
-//            }
-//
-//            @Override
-//            public void onDoNotRerouteToDestinationComplete() {
-//                //미리 종료 안내 동작 탐색 완료 시점에 호출
-//            }
-//
-//            @Override
-//            public void onDestinationDirResearchComplete() {
-//                // 건너편 안내 동작 탐색 완료 시점에 호출
-//            }
-//
-//            @Override
-//            public void onChangeRouteOptionComplete(@NonNull RoutePlanType routePlanType) {
-//                // 경로 옵션 변경 완료 시 호출
-//            }
-//
-//            @Override
-//            public void onBreakawayFromRouteEvent() {
-//                // 경로 이탈 재탐색 발생 시점에 호출
-//                Log.e(TAG, "onBreakawayFromRouteEvent");
-//            }
-//
-//            @Override
-//            public void onBreakAwayRequestComplete() {
-//                //경로 이탈 재탐색 동작 완료 시점에 호출
-//                Log.e(TAG, "onBreakAwayRequestComplete");
-//            }
-//
-//            @Override
-//            public void onArrivedDestination(@NonNull String dest, int drivingTime, int drivingDistance) {
-//                // 목적지 도착 시 호출
-//                // dest 목적지 명
-//                // drivingTime 운전시간
-//                // drivingDistance 운전거리
-//            }
-//
-//            @Override
-//            public void onApproachingViaPoint() {
-//                // 경유지 접근 시점에 호출 (1km 이내)
-//            }
-//
-//            @Override
-//            public void onApproachingAlternativeRoute() {
-//                // 대안 경로 접근 시 호출
-//            }
-//
-//            @Override
-//            public void onForceReroute(@NonNull com.skt.tmap.engine.navigation.network.ndds.NddsDataType.DestSearchFlag destSearchFlag)  {
-//                // 경로 재탐색 발생 시점에 호출
-//            }
-//        });
+        isEDC = false;
 
 
     }
