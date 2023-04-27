a16d543cb0033c4038b06913637f6cdbc376f772
hw20200500 <117514148+hw20200500@users.noreply.github.com>
2023-04-27 17:15:44 +0900

    마이페이지 추가

--- app/src/main/java/com/example/bigmap/MypageFragment.java
+++ app/src/main/java/com/example/bigmap/MypageFragment.java
@@ -31,4 +31,45 @@ public class MypageFragment extends Fragment {
 
         return inflater.inflate(R.layout.fragment_mypage, container, false);
     }
+    
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Button button_nameC = view.findViewById(R.id.button_nameC); // 버튼 id로 찾음
+        Button button_phoneC = view.findViewById(R.id.button_phoneC);
+        Button button_emailC = view.findViewById(R.id.button_emailC);
+        Button button_birthC = view.findViewById(R.id.button_birthC);
+
+        button_nameC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), mypage_name_ch.class); // 다른 Activity로 이동할 Intent 생성
+                startActivity(intent); // Intent 실행
+            }
+        });
+
+        button_phoneC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), mypage_phonnumCh.class); // 다른 Activity로 이동할 Intent 생성
+                startActivity(intent); // Intent 실행
+            }
+        });
+
+        button_emailC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), mypage_email_ch.class); // 다른 Activity로 이동할 Intent 생성
+                startActivity(intent); // Intent 실행
+            }
+        });
+
+        button_birthC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), mypage_birth_ch.class); // 다른 Activity로 이동할 Intent 생성
+                startActivity(intent); // Intent 실행
+            }
+        });
+    }
 }
