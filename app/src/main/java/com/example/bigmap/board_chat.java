d3e060fd5d615ec357e4f5174a68d8fb44bfa684
srriuhhh <miyn478@naver.com>
2023-05-02 17:30:55 +0900

    intent 및 클래스, xml 이름 수정, menifest 액티비티 추

---
+++ app/src/main/java/com/example/bigmap/board_chat.java
@@ -0,0 +1,18 @@
+package com.example.bigmap;
+
+import static android.app.PendingIntent.getActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class board_chat extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_board_chat);
+    }
+}
---
+++ app/src/main/java/com/example/bigmap/board_notice.java
@@ -0,0 +1,14 @@
+package com.example.bigmap;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class board_notice extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_board_notice);
+    }
+}
