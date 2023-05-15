cc66269c595e37297330b957e7ab171103d99acf
srriuhhh <miyn478@naver.com>
2023-05-15 18:22:07 +0900

    수정

---
+++ app/src/main/java/com/example/bigmap/board/board_free_detail.java
@@ -0,0 +1,34 @@
+package com.example.bigmap.board;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.skt.tmap.engine.navigation.network.NetworkRequester;
+
+
+
+import com.example.bigmap.R;
+
+public class board_free_detail extends AppCompatActivity {
+    private FirebaseAuth firebaseAuth;
+    private DatabaseReference databaseReference;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_board_free_detail);
+    }
+
+    Button btn_comment_reg = findViewById(R.id.btn_comment_reg);
+
+
+
+
+}
