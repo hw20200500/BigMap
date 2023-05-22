0529ca20aba1a00b6b214f9ecb48af8f67242bc9
ihyewon <loobis200172@gmail.com>
2023-05-22 19:28:35 +0900

    전화번호 edittext watchar 추가

--- app/src/main/java/com/example/bigmap/mypage/mypage_phonnumCh.java
+++ app/src/main/java/com/example/bigmap/mypage/mypage_phonnumCh.java
@@ -41,6 +41,7 @@ public class mypage_phonnumCh extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mypage_phonnum_ch);
 
+
         firebaseAuth = FirebaseAuth.getInstance();
         firebaseAuth.getFirebaseAuthSettings().setAppVerificationDisabledForTesting(true);
 
@@ -51,12 +52,13 @@ public class mypage_phonnumCh extends AppCompatActivity {
         Button buttonSend = findViewById(R.id.button_num_send);
         verificationCodeEditText = findViewById(R.id.phoneCertif_Num);
         editPhonenum = findViewById(R.id.userPhoneNum);
+        editPhonenum.addTextChangedListener(new PhoneNumberFormattingTextWatcher());
         buttonSend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String userPhoneNum = editPhonenum.getText().toString().trim();
                 // '-' 제거
-//                String mWithoutDash = userPhoneNum.replace("-", "");
+                String mWithoutDash = userPhoneNum.replace("-", "");
                 Random random = new Random();
                 int randomNumber = random.nextInt(999999);
 
@@ -78,7 +80,7 @@ public class mypage_phonnumCh extends AppCompatActivity {
                 try {
                     //전송
                     SmsManager smsManager = SmsManager.getDefault();
-                    smsManager.sendTextMessage(userPhoneNum, null, sms, null, null);
+                    smsManager.sendTextMessage(mWithoutDash, null, sms, null, null);
                     Toast.makeText(getApplicationContext(), "전송 완료!", Toast.LENGTH_LONG).show();
                 } catch (Exception e) {
                     Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_LONG).show();
